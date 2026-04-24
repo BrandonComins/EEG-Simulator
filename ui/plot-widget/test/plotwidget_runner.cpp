bd4832c2697ca3bc8b2dc5441cb79758f72ffd51
brandon <brandomin@gmail.com>
2026-04-23 23:46:12 -0700

    add more to runner for simple testing for now

--- ui/plot-widget/test/plotwidget_runner.cpp
+++ ui/plot-widget/test/plotwidget_runner.cpp
@@ -1,13 +1,49 @@
 #include <QApplication>
-#include "plotwidget.h"
+#include <QTimer>
+#include <QFrame>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <cmath>
+#include "plothelper.h"
+
+int timeouts = 0;
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    Plot::PlotWidget test_window;
+    constexpr int interval_ms = 15;
+    auto *timer = new QTimer();
+    timer->setInterval(interval_ms);
 
-    test_window.setWindowTitle("Plot Widget Runner");
-    test_window.show();
+    QWidget runner;
+    runner.setWindowTitle(QStringLiteral("Plot Runner"));
+    runner.resize(800, 600);
+
+    auto *main_layout = new QGridLayout(&runner);
+
+    auto *plot_container = new QFrame();
+    plot_container->setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
+    main_layout->addWidget(plot_container);
+
+    auto *plot_helper = new Plot::PlotHelper(plot_container);
+
+    plot_helper->add_series("Sine");
+    plot_helper->add_series("Cosine");
+    plot_helper->set_scale_x(0, 300);
+    plot_helper->set_axis_titles("Samples", "Amplitude");
+    plot_helper->set_view_mode(Plot::ViewMode::ROLLING);
+
+    QObject::connect(timer, &QTimer::timeout, [&]{
+        double sine_y = std::sin(timeouts * 0.1);
+        double cosine_y = std::cos(timeouts * 0.1);
+
+        plot_helper->add_point("Sine", static_cast<double>(timeouts), sine_y);
+        plot_helper->add_point("Cosine", static_cast<double>(timeouts), cosine_y);
+        timeouts += 1;
+    });
+
+    runner.show();
+    timer->start();
 
     return app.exec();
 }
