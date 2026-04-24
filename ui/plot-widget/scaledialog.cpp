d7c1e6536408a9f088a4d4dc3d904a79a7b48e7a
brandon <brandomin@gmail.com>
2026-04-23 23:44:12 -0700

    Add scale dialog

---
+++ ui/plot-widget/scaledialog.cpp
@@ -0,0 +1,39 @@
+#include "scaledialog.h"
+#include "ui_scaledialog.h"
+
+Plot::ScaleDialog::ScaleDialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::ScaleDialog) {
+
+    ui->setupUi(this);
+
+    this->setWindowTitle(QStringLiteral("Scale The Plot"));
+
+    QObject::connect(ui->pushButton_auto_scale, &QPushButton::clicked,
+                     this, &ScaleDialog::auto_scale_clicked, Qt::UniqueConnection);
+
+    QObject::connect(ui->pushButton_scale, &QPushButton::clicked,
+                     this, &ScaleDialog::handle_scale_button, Qt::UniqueConnection);
+}
+
+Plot::ScaleDialog::~ScaleDialog() {
+    delete ui;
+}
+
+void Plot::ScaleDialog::ScaleDialog::handle_scale_button() {
+    double min_x = ui->doubleSpinBox_min_x->value();
+    double max_x = ui->doubleSpinBox_max_x->value();
+    double min_y = ui->doubleSpinBox_min_y->value();
+    double max_y = ui->doubleSpinBox_max_y->value();
+
+    if (min_x > max_x) {
+        std::swap(min_x, max_x);
+    }
+
+    if (min_y > max_y) {
+        std::swap(min_y, max_y);
+    }
+
+
+    Q_EMIT scale_clicked(min_x, max_x, min_y, max_y);
+}
---
+++ ui/plot-widget/scaledialog.h
@@ -0,0 +1,53 @@
+#ifndef SCALEDIALOG_H
+#define SCALEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class ScaleDialog;
+}
+
+namespace Plot {
+
+class ScaleDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    /*!
+     * \brief Constructs a new ScaleDialog object.
+     * \param parent The parent widget.
+     */
+    explicit ScaleDialog(QWidget *parent = nullptr);
+
+    /*!
+     * \brief Destroys the ScaleDialog.
+     */
+    ~ScaleDialog() override;
+
+    /*!
+     * \brief Collects values from the UI fields and emits the scale_clicked signal.
+     */
+    void handle_scale_button();
+
+Q_SIGNALS:
+    /*!
+     * \brief Signal emitted when the user confirms manual scale values.
+     * \param min_x The minimum value for the horizontal axis.
+     * \param max_x The maximum value for the horizontal axis.
+     * \param min_y The minimum value for the vertical axis.
+     * \param max_y The vertical axis maximum value.
+     */
+    void scale_clicked(double min_x, double max_x, double min_y, double max_y);
+
+    /*!
+     * \brief Signal emitted when the user requests an automatic best-fit scaling.
+     */
+    void auto_scale_clicked();
+
+private:
+    Ui::ScaleDialog *ui; //!< Pointer to the UI object
+};
+} //namespace Plot
+
+#endif // SCALEDIALOG_H
